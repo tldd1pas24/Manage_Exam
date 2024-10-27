db1da64be5ed069323ee7ffa1c0cb4950806dabe
KhaThach <khathach699@gmail.com>
2024-10-27 13:48:23 +0700

    add file TblCauHoiMa, TblChiTietCauHoiMa in shared 27/10/2024

---
+++ ExamManager/src/Hutech.Exam/Shared/Models/TblChiTietCauHoiMa.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hutech.Exam.Shared.Models;
+
+public partial class TblChiTietCauHoiMa
+{
+    public int MaCauHoiMa { get; set; }
+
+    public long MaChiTietBaiThi { get; set; }
+}
